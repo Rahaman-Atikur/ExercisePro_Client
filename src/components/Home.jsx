befa2af47cf7ea80af5cb0b2ee53f3b267891aa6
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-02-05 23:21:31 +0000

    Front end code is ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2 className='text-7xl font-extrabold'>Hi This is Home Page</h2>
+            <h2 className='text-7xl font-extrabold'>Input all the Information </h2>
+            <input type="text" placeholder="Name" className="input" />
+            <br />
+            <input type="text" placeholder="Id" className="input input-lg" /> 
+            <br />
+            <input type="text" placeholder="Age" className="input input-lg" />
+            <br />
+            <input type="text" placeholder="City" className="input input-lg" />
+            <br />
+            <input type="text" placeholder="Country" className="input input-lg" />
+            <br />
+            <input type="text" placeholder="Large" className="input input-lg" />
+            <br />
+
+            <input type="submit" value="Submit" className="btn" />
+           
         </div>
     );
 };
