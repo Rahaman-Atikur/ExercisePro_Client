db6f33806d1d76596e73de48a54d49ac33aa5179
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-02-05 23:39:00 +0000

    Front end is ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 
 const Home = () => {
+    const handleInput = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+       const data = Object.fromEntries(formData.entries());
+        console.log("Form Data:", data);
+    };
     return (
         <div>
-            <h2 className='text-7xl font-extrabold'>Input all the Information </h2>
-            <input type="text" placeholder="Name" className="input" />
+            <form onSubmit={handleInput} className='w-1/2 mx-auto mt-10'>
+
+
+                <h2 className='text-7xl font-extrabold'>Input all the Information </h2>
+            <input  name='name' type="text" placeholder="Name" className="input" />
             <br />
-            <input type="text" placeholder="Id" className="input input-lg" /> 
+            <input name='id'  type="text" placeholder="Id" className="input input-lg" /> 
             <br />
-            <input type="text" placeholder="Age" className="input input-lg" />
+            <input  name='age' type="text" placeholder="Age" className="input input-lg" />
             <br />
             <input type="text" placeholder="City" className="input input-lg" />
             <br />
-            <input type="text" placeholder="Country" className="input input-lg" />
+            <input name='country' type="text" placeholder="Country" className="input input-lg" />
             <br />
-            <input type="text" placeholder="Large" className="input input-lg" />
+            <input name='textBox' type="text" placeholder="Large" className="input input-lg" />
             <br />
 
             <input type="submit" value="Submit" className="btn" />
+            </form>
+            
            
         </div>
     );
